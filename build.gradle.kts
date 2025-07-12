ce2207da235f325bd9a4234b2934a68478887495
YashChaudharyGitHub <87370389+YashChaudharyGitHub@users.noreply.github.com>
2025-07-13 05:21:31 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("cloudstream-plugin")
+}
+
+cloudstream {
+    language = "en"
+    description = "Watch live and upcoming sports via streamed.su API."
+    authors = listOf("YashChaudharyGitHub")
+}
